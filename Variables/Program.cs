569fcf3b1d14aa9767d67d2fa5834a89fae47baf
Recep Yolcu <73476864+Recepyolcu@users.noreply.github.com>
2022-09-11 21:27:55 +0300

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var localTime = DateTime.Now;
+            int localYear = localTime.Year;
+
+            Console.Write("Enter your birth year: ");
+            int birthYear = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Your age is: {0}", (localYear - birthYear));
+
+            Console.ReadKey();
+            
+        }
+    }
+}
